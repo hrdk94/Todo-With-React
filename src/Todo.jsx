46bc030ca425d89960f9f9be8d13939526f93c9b
hrdk94 <goyalhardik220@gmail.com>
2025-07-05 18:38:44 +0530

    template

---
+++ src/Todo.jsx
@@ -0,0 +1,11 @@
+function Todo(){
+    return(
+        <div>
+            <input type="text" placeholder="Write your tasks here..." />
+            <button>ADD TASK</button>
+            <p>TODO LIST!</p>
+        </div>
+    )
+}
+
+export default Todo;
