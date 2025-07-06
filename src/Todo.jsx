c219128a34e595817beed2c0947ea261c063a1bb
hrdk94 <goyalhardik220@gmail.com>
2025-07-06 23:27:47 +0530

    Done funct

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,13 +3,13 @@ import { v4 as uuidv4 } from 'uuid'
 
 
 function Todo(){
-    let [todos, setTodos] = useState([{task: "sample task", id: uuidv4()}]);
+    let [todos, setTodos] = useState([{task: "sample task", id: uuidv4(), isDone: false}]);
     let [newTodo, setNewTodo] = useState("");
 
 
     let AddNewTask = () => {
         setTodos((prevTodos) => {
-            return [...prevTodos, {task: newTodo, id: uuidv4()}]
+            return [...prevTodos, {task: newTodo, id: uuidv4(), isDone: false}]
         });
         setNewTodo("");
     };
@@ -65,6 +65,20 @@ let  LowerCaseOne = (id) =>{
 }
 
 
+let TaskDone = (id) => {
+  setTodos(todos.map((todo) => {
+    if (todo.id === id) {
+      return {
+        ...todo,
+        isDone: !todo.isDone
+      };
+    } else {
+      return todo;
+    }
+  }));
+};
+
+
 
     return(
         <div>
@@ -85,9 +99,10 @@ let  LowerCaseOne = (id) =>{
             <ul>
                 {
                     todos.map((todos)=>(
-                        <li key={todos.id}>
+                        <li key={todos.id} style={{ textDecoration: todos.isDone ? "line-through" : "none" }}>
                             {todos.task}
                             <button style={{margin: "8px"}} onClick={()=>DeleteTodo(todos.id)}>Delete</button>
+                            <button onClick={() => TaskDone(todos.id)}>Done!</button>
                             <button style={{margin: "8px"}} onClick={() => UpperCaseOne(todos.id)}>UpperCase One</button>
                             <button onClick={() => LowerCaseOne(todos.id)}>LowerCase One</button>
                             </li>          
